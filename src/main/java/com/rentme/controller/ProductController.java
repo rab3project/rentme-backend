82088d71eba2747ebb63455d6ccc15b0c27d71ae
anushaprajapati <prz.anusha@gmail.com>
2019-11-20 12:00:44 -0500

    Product Api

---
+++ src/main/java/com/rentme/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.rentme.controller;
+
+import com.rentme.model.Product;
+import com.rentme.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @RequestMapping(value = "/productinfo", method = RequestMethod.POST)
+    public void createProduct(@Valid @RequestBody Product product){
+        productRepository.save(product);
+
+    }
+
+}
---
+++ src/main/java/com/rentme/model/Product.java
@@ -0,0 +1,75 @@
+package com.rentme.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "productId")
+    private long ProductId;
+
+    @Column(name= "name")
+    private String Name;
+
+    @Column(name = "description")
+    private String Description;
+
+    private Date date;
+
+    private double PricePerDay;
+
+    public Product() {
+    }
+
+    public long getProductId() {
+
+        return ProductId;
+    }
+
+    public void setProductId(long productId) {
+
+        ProductId = productId;
+    }
+
+    public String getName() {
+
+        return Name;
+    }
+
+    public void setName(String name) {
+
+        Name = name;
+    }
+
+    public String getDescription() {
+
+        return Description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setDescription(String description) {
+
+        Description = description;
+    }
+
+    public double getPricePerDay() {
+
+        return PricePerDay;
+    }
+
+    public void setPricePerDay(double pricePerDay) {
+
+        PricePerDay = pricePerDay;
+    }
+}
