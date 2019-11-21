9f0848c1d031a81c7192bee567dc8bcb94e62191
Abhi <manandharabhi@gmail.com>
2019-11-21 10:25:35 -0500

    costumerController

--- src/main/java/com/rentme/controller/CustomerController.java
+++ src/main/java/com/rentme/controller/CustomerController.java
@@ -14,7 +14,7 @@ import com.rentme.dtos.CustomerSignupDto;
 @RequestMapping("/customer")
 public class CustomerController {
 
-	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
 	public void signup(@RequestBody CustomerSignupDto signupDto) {
 		
 	}
